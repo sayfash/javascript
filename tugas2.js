821b672b7a97d331745562ebec00b5bfa8294171
sayfash <ahmadsaif.ash@gmail.com>
2023-10-22 05:18:19 +0700

    Add tugas2.js

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+let myNumber = 23;
+let myString = "Nama Saya Budi";
+let myBoolean = true;
+let myNol = null;
+let myUndef = undefined;
+let myArray = [[2, "Budi"], ["Henry", 36]];
+
+console.log(myNumber);
+console.log(myString);
+console.log(myBoolean);
+console.log(myNol);
+console.log(myUndef);
+console.log(myArray);
