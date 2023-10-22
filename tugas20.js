4dfc35528de55a966d278457e44ef3afca29c576
sayfash <ahmadsaif.ash@gmail.com>
2023-10-22 20:03:43 +0700

    Add tugas20.js

---
+++ tugas20.js
@@ -0,0 +1,8 @@
+let myStr =  "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+
+function matchStr(x) {
+    console.log(myStr.match(x));
+}
+
+matchStr("are");
+matchStr("words");
