56d6e199122b52be6060c9f9f202269ec69aab5a
sayfash <ahmadsaif.ash@gmail.com>
2023-10-22 19:34:25 +0700

    Add tugas19.js

---
+++ tugas19.js
@@ -0,0 +1,18 @@
+let myStr = "Semangat...";
+let strOne = "*";
+
+function repeatStr(x, y) {
+    console.log(x.repeat(y));
+}
+
+repeatStr(myStr, 5);
+console.log("");
+repeatStr(strOne, 5);
+repeatStr(strOne, 25);
+repeatStr(strOne, 45);
+repeatStr(strOne, 75);
+repeatStr(strOne, 105);
+repeatStr(strOne, 75);
+repeatStr(strOne, 45);
+repeatStr(strOne, 25);
+repeatStr(strOne, 5);
