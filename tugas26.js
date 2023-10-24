2205b13f81b8a9972290c9abebcbc120250a36cc
sayfash <ahmadsaif.ash@gmail.com>
2023-10-24 22:39:43 +0700

    Add tugas26.js

---
+++ tugas26.js
@@ -0,0 +1,19 @@
+function checkFinite(x){
+    if (isFinite(x)) {
+        console.log("Angka " + x + " NOT Infinity");
+    } else {
+        console.log("Infinity");
+    }
+}
+
+checkFinite(2);
+checkFinite(39);
+checkFinite(76);
+checkFinite(50);
+checkFinite(9);
+checkFinite(7);
+checkFinite(41);
+checkFinite(2);
+checkFinite(24);
+checkFinite(1);
+checkFinite(16);
