bd246651600adb33e62e41b9cb2d810f64bcf5db
sayfash <ahmadsaif.ash@gmail.com>
2023-10-25 15:57:18 +0700

    Add tugas27.js

---
+++ tugas27.js
@@ -0,0 +1,15 @@
+let a = "No ";
+let b = "rumah saya ";
+let c = 30;
+let d = "A";
+
+let e = a + b + String(c) + d;
+console.log(e);
+
+let f = "Jalan pangeran Tirtayasa No ";
+let g = 70;
+
+let h = f + String(g);
+console.log(h);
+
+console.log(e + ". " + h + ".");
